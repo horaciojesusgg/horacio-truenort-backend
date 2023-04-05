302b5eced6ba2b8839687617e6ea7f1e8388904c
Horacio Gonzalez <horacio@quipli.com>
2023-04-05 13:46:51 -0300

    remove redis files

--- src/record/query/getAllRecords.query.ts
+++
@@ -1,9 +0,0 @@
-import { autoInjectable, inject } from 'tsyringe';
-import { Redis as RedisClient } from 'ioredis';
-
-@autoInjectable()
-export default class getAllRecordsQuery {
-  constructor(@inject('RedisClient') private readonly redisClient: RedisClient) {}
-
-  execute() {}
-}
